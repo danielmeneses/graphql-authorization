c76b6151899de99605c32dd386db499022e1108c
danielmeneses <danielmeneses.pt@gmail.com>
2018-05-05 02:14:36 +0100

    [MINOR] updated tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -11,6 +11,8 @@ test('Parse query - validate rules tree', () => {
         author: "Daniel"
         data1: [0]
         data2: [{id: 2}]
+        data3: [{id: [3]}]
+        data4: [{id: [{id: 4}]}]
       ) {
         id
         author(input: {
@@ -28,11 +30,14 @@ test('Parse query - validate rules tree', () => {
   expect(parsedRules).toBeInstanceOf(Object);
 
   // validate tree values
+  // console.log(_in.data2['0']);
   const _in = parsedRules.query.$out.books.$in;
   expect(_in.id[INPUT_VAL_KEY]).toEqual(1);
   expect(_in.author[INPUT_VAL_KEY]).toEqual('Daniel');
-  expect(_in.data1['0'].$in[INPUT_VAL_KEY]).toEqual(0);
+  expect(_in.data1['0'][INPUT_VAL_KEY]).toEqual(0);
   expect(_in.data2['0'].$in.id[INPUT_VAL_KEY]).toEqual(2);
+  expect(_in.data3['0'].$in.id['0'][INPUT_VAL_KEY]).toEqual(3);
+  expect(_in.data4['0'].$in.id['0'].$in.id[INPUT_VAL_KEY]).toEqual(4);
 
   const _out = parsedRules.query.$out.books.$out;
   expect(_out.author.$in.input.$in.id[INPUT_VAL_KEY]).toEqual(123);
